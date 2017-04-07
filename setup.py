2ad9e481cd1e3b7658766bfb822b446656cc86af
Mikhail <martmiel@gmail.com>
2017-04-07 13:48:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Daniel Liu, Volodymyr Rospotniuk',
   author_email = 'hpec.liu@gmail.com, rospotniuk@gmail.com',
   url = 'https://github.com/rospotniuk/test_helper',
-  download_url = 'https://github.com/rinnyB/test_helper/tarball/0.15',
+  download_url = 'https://github.com/rinnyB/test_helper/tarball/0.16',
   keywords = ['testing', 'autograder', 'image'],
   classifiers = [],
   include_package_data = True
